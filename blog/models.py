decedf9b79558a3b049d9e5b00399dc4ed96201f
nopibal <ytneirby1@gmail.com>
2021-10-13 13:07:44 +0300

    fix datetime

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=110)
-    date = models.DateField(auto_now=True)
-    description = models.CharField(max_length=250)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
